4683aba119e88e9f6cd789c3cb39b7a030139b57
zyppayx-lab <zyppayx@gmail.com>
2026-04-25 00:11:40 +0800

    Delete src/config directory

--- src/config/db.js
+++
@@ -1,6 +0,0 @@
-const { Pool } = require('pg');
-const env = require('./env');
-
-module.exports = new Pool({
-  connectionString: env.DATABASE_URL
-});
--- src/config/redis.js
+++
@@ -1,3 +0,0 @@
-const Redis = require('ioredis');
-const env = require('./env');
-module.exports = new Redis(env.REDIS_URL);
