e03f47f432bd6d45748e8aa7868da88f7ab96594
zyppayx-lab <zyppayx@gmail.com>
2026-04-27 20:58:16 +0800

    Enhance createTask with userId and fee validation
    Added userId and feePaid parameters to createTask function. Implemented fee validation for task creation.

--- src/modules/tasks/task.service.js
+++ src/modules/tasks/task.service.js
@@ -1,8 +1,22 @@
 // src/modules/tasks/task.service.js
 
 const db = require('../../config/db');
+const taskPayment = require('./taskPayment.service');
 
-exports.createTask = async ({ businessId, title, description, reward, slots }) => {
+exports.createTask = async ({
+  businessId,
+  userId,
+  title,
+  description,
+  reward,
+  slots,
+  feePaid
+}) => {
+
+  if (!feePaid) {
+    throw new Error('Task creation fee not paid');
+  }
+
   const res = await db.query(
     `INSERT INTO tasks(business_id, title, description, reward, slots, status)
      VALUES ($1,$2,$3,$4,$5,'PENDING_APPROVAL')
