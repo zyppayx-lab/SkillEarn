0acf2d13f940c51c7dc9703559590dc7c44b807b
zyppayx-lab <zyppayx@gmail.com>
2026-04-27 20:44:53 +0800

    Add walletType parameter to credit and debit methods

--- src/modules/wallets/wallet.service.js
+++ src/modules/wallets/wallet.service.js
@@ -2,20 +2,22 @@
 
 const ledger = require('../../core/ledger.service');
 
-exports.credit = async ({ userId, amount, ref }) => {
+exports.credit = async ({ userId, amount, ref, type }) => {
   return ledger.postEntry({
     userId,
     type: 'credit',
     amount,
-    reference: ref
+    reference: ref,
+    walletType: type // USER or BUSINESS
   });
 };
 
-exports.debit = async ({ userId, amount, ref }) => {
+exports.debit = async ({ userId, amount, ref, type }) => {
   return ledger.postEntry({
     userId,
     type: 'debit',
     amount,
-    reference: ref
+    reference: ref,
+    walletType: type
   });
 };
