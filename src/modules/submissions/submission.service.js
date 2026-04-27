130d0775d1ce90420d73c90ba0dc245ba313e24b
zyppayx-lab <zyppayx@gmail.com>
2026-04-27 21:02:52 +0800

    Add submission functionality to insert proof

--- src/modules/submissions/submission.service.js
+++ src/modules/submissions/submission.service.js
@@ -3,6 +3,7 @@
 const db = require('../../config/db');
 
 exports.submitProof = async ({ taskId, userId, proof }) => {
+
   const res = await db.query(
     `INSERT INTO submissions(task_id, user_id, proof, status)
      VALUES ($1,$2,$3,'PENDING')
